444228a797dfe2d4b17b55297f8189a205e514f0
闫煜瑶 <y.yy@winning.com.cn>
2024-02-20 09:00:07 +0800

    修改切换样式

--- app.vue
+++ app.vue
@@ -9,19 +9,19 @@
 </template>
 <script setup lang="ts">
 useHead({
-  // 作为字符串，
-  // 其中`%s`会被标题替换
-  titleTemplate: '网站标题'
+	// 作为字符串，
+	// 其中`%s`会被标题替换
+	titleTemplate: '网站标题'
 })
 </script>
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.4s;
+	transition: all 0.4s;
 }
 .page-enter-from,
 .page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
+	opacity: 0;
+	filter: blur(1rem);
 }
 </style>
